fb914f0cf4391e408326d811cbccdbe2cdda3cd5
mattmc3 <mattmc3@gmail.com>
2020-09-30 23:17:24 -0400

    Only cd-ls in interactive shells

--- fish/.config/fish/conf.d/cd-ls.fish
+++ fish/.config/fish/conf.d/cd-ls.fish
@@ -2,7 +2,7 @@ set -q LS_AFTER_CD || set -xg LS_AFTER_CD true
 
 # show directory listing on directory change
 function __ls_after_cd__on_variable_pwd --on-variable PWD
-    if test "$LS_AFTER_CD" = true
+    if test "$LS_AFTER_CD" = true; and status --is-interactive
         ls -GF
     end
 end
