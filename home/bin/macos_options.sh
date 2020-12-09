12dfe1c920da47a7054d4266e73675a6a7f2bb97
mattmc3 <mattmc3@gmail.com>
2020-12-09 15:28:27 -0500

    Update macos_options.sh

--- home/bin/macos_options.sh
+++ home/bin/macos_options.sh
@@ -1,20 +1,30 @@
 #!/usr/bin/env bash
 
-# .macos: https://github.com/mathiasbynens/dotfiles/blob/master/.macos
+# dotfiles references
+# https://github.com/pawelgrzybek/dotfiles/blob/master/setup-macos.sh
+# https://github.com/mathiasbynens/dotfiles/blob/master/.macos
+# https://github.com/geerlingguy/dotfiles/blob/master/.osx
 
+
+# --- Keyboard ---
 # Disable press-and-hold for keys in favor of key repeat
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Or, do this for individual apps
 # defaults write com.sublimetext.3 ApplePressAndHoldEnabled -bool false
 # defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
-# # reset back to default
+
+# if you need to reset back to default
 # defaults delete -g ApplePressAndHoldEnabled
 
-# Save screenshots to the downloads
-mkdir -p "$HOME/Pictures/Screenshots"
-defaults write com.apple.screencapture location -string "$HOME/Pictures/Screenshots"
 
+# --- Folders ---
+# Save screenshots to the downloads
+mkdir -p "$HOME/Screenshots"
+defaults write com.apple.screencapture location -string "$HOME/Screenshots"
+
+
+# --- Desktop & Screen Saver ---
 # Hot corners
 # Possible values:
 #  0: no-op
@@ -30,5 +40,13 @@ defaults write com.apple.screencapture location -string "$HOME/Pictures/Screensh
 defaults write com.apple.dock wvous-br-corner -int 5
 defaults write com.apple.dock wvous-br-modifier -int 0
 
+
+# --- Misc ---
 # why does Mojave font rendering suck on external monitors? FixedIt!
 defaults write -g CGFontRenderingFontSmoothingDisabled -bool NO
+
+
+# --- Finder Preferences ---
+
+# Finder > View > Show Path Bar
+defaults write com.apple.finder ShowPathbar -bool true
