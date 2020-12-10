a8003619bd146133b7607bac544756d8e2cf23ab
mattmc3 <mattmc3@gmail.com>
2020-12-09 23:47:54 -0500

    Update macos_options.sh

--- bin/macos_options.sh
+++ bin/macos_options.sh
@@ -1,9 +1,25 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 # dotfiles references
 # https://github.com/pawelgrzybek/dotfiles/blob/master/setup-macos.sh
 # https://github.com/mathiasbynens/dotfiles/blob/master/.macos
 # https://github.com/geerlingguy/dotfiles/blob/master/.osx
+
+
+# --- Dock ---
+# https://developer.apple.com/documentation/devicemanagement/dock
+
+# System Preferences > Dock > Magnification:
+defaults write com.apple.dock magnification -bool true
+
+# Minimize windows into their application’s icon
+defaults write com.apple.dock minimize-to-application -bool true
+
+# Change minimize/maximize window effect from genie to scale
+defaults write com.apple.dock mineffect -string "scale"
+
+# move to left
+defaults write com.apple.dock orientation left
 
 
 # --- Keyboard ---
@@ -50,3 +66,24 @@ defaults write -g CGFontRenderingFontSmoothingDisabled -bool NO
 
 # Finder > View > Show Path Bar
 defaults write com.apple.finder ShowPathbar -bool true
+
+# --- Restart affected apps ---
+
+for app in \
+  "Activity Monitor" \
+  "Address Book" \
+  "Calendar" \
+  "cfprefsd" \
+  "Contacts" \
+  "Dock" \
+  "Finder" \
+  "Mail" \
+  "Messages" \
+  "Photos" \
+  "Safari" \
+  "SystemUIServer" \
+  "Terminal" \
+  "iCal"; do
+  killall "${app}" &> /dev/null
+done
+echo "Done. Note that some of these changes require a logout/restart to take effect."
