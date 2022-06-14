6917d73671a2fcdf20c14aeb31ff56132408c186
mattmc3 <mattmc3@gmail.com>
2022-06-14 11:49:42 -0400

    Add rational emacs

---
+++ .config/rational-emacs/early-config.el
@@ -0,0 +1,11 @@
+;; set the initial window position an UI elements
+(add-to-list 'default-frame-alist '(width . 120))
+
+;; (setq default-frame-alist
+;;       '((height . 30)
+;;         (width . 100)
+;;         (left . 150)
+;;         (top . 100)
+;;         (vertical-scroll-bars . nil)
+;;         (horizontal-scroll-bars . nil)
+;;         (tool-bar-lines . 0)))
