3e366e7bdf567ee4adbb171669c524b333d788c5
mattmc3 <mattmc3@gmail.com>
2022-09-22 09:17:47 -0400

    Move hist file

--- config/bash/bashrc.bash
+++ config/bash/bashrc.bash
@@ -26,7 +26,8 @@ HISTTIMEFORMAT='%F %T '   # use standard ISO 8601 timestamp
 HISTSIZE=100000           # remember the last x commands in memory during session
 HISTFILESIZE=100000       # start truncating history file after x lines
 HISTCONTROL=ignoreboth    # ignoreboth is shorthand for ignorespace and ignoredups
-HISTFILE=$HOME/.bash_history
+HISTFILE=$XDG_DATA_HOME/bash/history
+[[ -f $HISTFILE ]] || mkdir -p $(dirname $HISTFILE)
 PROMPT_COMMAND="history -a;history -c;history -r;$PROMPT_COMMAND"
 
 
