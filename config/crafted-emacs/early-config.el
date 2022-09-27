07719197e85fc5ee4fd381f5c21241ab4570e83c
mattmc3 <mattmc3@gmail.com>
2022-09-27 12:14:22 -0400

    Update initial frame params

--- config/crafted-emacs/early-config.el
+++ config/crafted-emacs/early-config.el
@@ -1,11 +1,19 @@
-;; set the initial window position an UI elements
-(add-to-list 'default-frame-alist '(width . 120))
+;;(defun add-list-to-list (dst src)
+;;   "Similar to `add-to-list', but accepts a list as 2nd argument"
+;;   (set dst
+;;        (append (eval dst) src)))
 
-;; (setq default-frame-alist
-;;       '((height . 30)
-;;         (width . 100)
-;;         (left . 150)
-;;         (top . 100)
-;;         (vertical-scroll-bars . nil)
-;;         (horizontal-scroll-bars . nil)
-;;         (tool-bar-lines . 0)))
+;; ;; set the initial window position an UI elements
+;; (add-list-to-list 'default-frame-alist
+;;                   '((width . 120)
+;;                     (left . 150)
+;;                     (top . 100)))
+
+(setq default-frame-alist
+      '((height . 30)
+        (width . 100)
+        (left . 150)
+        (top . 100)
+        (vertical-scroll-bars . nil)
+        (horizontal-scroll-bars . nil)
+        (tool-bar-lines . 0)))
