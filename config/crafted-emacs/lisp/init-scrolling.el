c9c1a434b345886e9e120382246f5bb9eb65dd6a
mattmc3 <mattmc3@gmail.com>
2022-10-12 12:51:05 -0400

    Move my stuff into lisp folder

---
+++ config/crafted-emacs/lisp/init-scrolling.el
@@ -0,0 +1,14 @@
+;; -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;;
+;; Fish shell syntax.
+;;
+
+;;; Code:
+
+;; https://github.com/io12/good-scroll.el
+(crafted-package-install-package 'good-scroll)
+
+(provide 'init-scrolling)
