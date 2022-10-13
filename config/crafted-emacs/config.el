3da07ec06e89649bbede7946cd3d2ab5563ab227
mattmc3 <mattmc3@gmail.com>
2022-10-13 15:11:27 -0400

    Update config

--- config/crafted-emacs/config.el
+++ config/crafted-emacs/config.el
@@ -18,20 +18,25 @@
 ;; evil-mode, as it will greatly change how you interact with Emacs.
 ;; So, if you prefer Vim-style keybindings over vanilla Emacs keybindings
 ;; remove the comment in the line about `crafted-evil' below.
-;; (require 'crafted-defaults)    ; Sensible default settings for Emacs
-;; (require 'crafted-updates)     ; Tools to upgrade Crafted Emacs
-;; (require 'crafted-completion)  ; selection framework based on `vertico`
-;; (require 'crafted-ui)          ; Better UI experience (modeline etc.)
-;; (require 'crafted-ide)         ; Eglot configuration
-;; (require 'crafted-windows)     ; Window management configuration
-;; (require 'crafted-editing)     ; Whitspace trimming, auto parens etc.
-;; ;(require 'crafted-evil)        ; An `evil-mode` configuration
-;; (require 'crafted-org)         ; org-appear, clickable hyperlinks etc.
-;; (require 'crafted-project)     ; built-in alternative to projectile
+(require 'crafted-defaults)    ; Sensible default settings for Emacs
+(require 'crafted-updates)     ; Tools to upgrade Crafted Emacs
+(require 'crafted-completion)  ; selection framework based on `vertico`
+(require 'crafted-ui)          ; Better UI experience (modeline etc.)
+(require 'crafted-ide)         ; Eglot configuration
+(require 'crafted-windows)     ; Window management configuration
+(require 'crafted-editing)     ; Whitspace trimming, auto parens etc.
+;; (require 'crafted-evil)        ; An `evil-mode` configuration
+(require 'crafted-org)         ; org-appear, clickable hyperlinks etc.
+(require 'crafted-project)     ; built-in alternative to projectile
 ;; (require 'crafted-speedbar)    ; built-in file-tree
-;; (require 'crafted-screencast)  ; show current command and binding in modeline
-;; ;;(require 'crafted-compile)     ; automatically compile some emacs lisp files
+(require 'crafted-screencast)  ; show current command and binding in modeline
+;; (require 'crafted-compile)     ; automatically compile some emacs lisp files
 
+;;; Package Management
+(add-to-list 'load-path (expand-file-name "lisp" crafted-config-path))
+
+(require 'init-package)
+(require 'init-commenting)
 (require 'init-cursor)
 (require 'init-fish)
 (require 'init-mouse)
@@ -39,9 +44,10 @@
 (require 'init-org)
 (require 'init-osx)
 (require 'init-scratch)
-(require 'init-tabline)
+(require 'init-tabs)
 (require 'init-theme)
-(require 'init-xah-fly-keys)
+(require 'init-scrolling)
+;;(require 'init-xah-fly-keys)
 ;;(require 'init-meow)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
--- config/crafted-emacs/custom.el
+++ config/crafted-emacs/custom.el
@@ -4,10 +4,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(doom-themes vertico org-appear orderless multiple-cursors modus-themes marginalia keycast helpful embark-consult elisp-demos doom-modeline corfu-doc cape all-the-icons)))
+   '(persistent-scratch doom-themes vertico org-appear orderless multiple-cursors modus-themes marginalia keycast helpful embark-consult elisp-demos doom-modeline corfu-doc cape all-the-icons)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:font "MesloLGL Nerd Font 13"))))
+ '(fixed-pitch ((t (:inherit (default)))))
+ '(fixed-pitch-serif ((t (:inherit (default)))))
+ '(variable-pitch ((t (:font "Arial 18")))))
