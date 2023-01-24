38cbe2c01b4f81e1f15b04847be71ab45a157ef3
mattmc3 <mattmc3@gmail.com>
2023-01-24 08:17:30 -0500

    Update env

--- home/dot-zsh/conf.d/01-env.zsh
+++ home/dot-zsh/conf.d/01-env.zsh
@@ -1,5 +1,28 @@
-if [[ "$OSTYPE" == darwin* ]]; then
-  export SHELL_SESSIONS_DISABLE=1
-  export CLICOLOR=1
-fi
+export EDITOR='vim'
+export VISUAL='vim'
+export PAGER='less'
+export LANG='en_US.UTF-8'
+
+# ensure path arrays do not contain duplicates
+typeset -gU path fpath cdpath
+
+path=(
+  $HOME/{,s}bin(N)
+  /opt/{homebrew,local}/{,s}bin(N)
+  /usr/local/{,s}bin(N)
+  $path
+)
+
+# /usr/bin/less --help
+export LESS='-g -i -M -R -S -w -z-4'
+
+# Use `< file` to quickly view the contents of any file.
+READNULLCMD=$PAGER
+
+# Go fast
 KEYTIMEOUT=1
+
+if [[ "$OSTYPE" == darwin* ]]; then
+  export BROWSER='open'
+  export SHELL_SESSIONS_DISABLE=1
+fi
