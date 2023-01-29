3cce95d9f43d7600fc934e1539a49e3697858c1e
mattmc3 <mattmc3@gmail.com>
2023-01-28 21:12:22 -0500

    Remove dot-zsh

--- home/dot-zsh/conf.d/99-local.zsh
+++
@@ -1,5 +0,0 @@
-# local overrides
-[[ ! -f $ZDOTDIR/.zshrc_local ]] || source $ZDOTDIR/.zshrc_local
-
-# dotfiles local overrides from dotfiles
-[[ ! -f $DOTFILES.local/zsh/zshrc_local.zsh ]] || source $DOTFILES.local/zsh/zshrc_local.zsh
