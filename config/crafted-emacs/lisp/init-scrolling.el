239b8fbe43c0881f79eb16534c13291878a58214
mattmc3 <mattmc3@gmail.com>
2023-08-27 20:43:27 -0400

    Remove unused

--- config/crafted-emacs/lisp/init-scrolling.el
+++
@@ -1,8 +0,0 @@
-;;; init-scrolling.el --- Better scrolling -*- lexical-binding: t -*-
-;;; Commentary:
-;;; Code:
-
-;; https://github.com/io12/good-scroll.el
-(use-package good-scroll)
-
-(provide 'init-scrolling)
