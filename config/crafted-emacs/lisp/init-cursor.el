f2ed69525f83e6250fe92c519906238ab61efdff
mattmc3 <mattmc3@gmail.com>
2023-08-30 22:17:10 -0400

    Remove crafted-emacs

--- config/crafted-emacs/lisp/init-cursor.el
+++
@@ -1,8 +0,0 @@
-;;; init-cursor.el -*- lexical-binding: t -*-
-;;; Commentary:
-;;; Code:
-
-;; Make the cursor a bar, not a blinking box
-(setq-default cursor-type 'bar)
-
-(provide 'init-cursor)
--- config/crafted-emacs/lisp/init-xah-fly-keys.el
+++
@@ -1,7 +0,0 @@
-
-(crafted-package-install-package 'xah-fly-keys)
-(require 'xah-fly-keys)
-(xah-fly-keys-set-layout "colemak")
-(xah-fly-keys 1)
-(global-set-key (kbd "<f4>") 'xah-fly-mode-toggle)
-(provide 'init-xah-fly-keys)
