8c4820b056441ccc121017394cfb160bbbbb5cda
mattmc3 <mattmc3@gmail.com>
2024-08-17 23:25:49 -0400

    Update wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,8 +6,36 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
--- For example, changing the color scheme:
+-- What's our shell?
+shell = '/opt/homebrew/bin/fish'
+config.default_prog = { shell }
+config.set_environment_variables = {
+	SHELL = shell,
+}
+
+-- Change the color scheme
 config.color_scheme = 'Wombat'
+
+config.macos_window_background_blur = 20
+config.window_background_opacity = 0.9
+-- config.background = {
+-- 	{
+-- 		source = {
+-- 			Color = '#0f1019'
+-- 		},
+-- 		width = "100%",
+-- 		height = "100%",
+-- 		opacity = .9
+-- 	},
+-- }
+
+config.colors = {
+	-- foreground = '#8f92a0',
+	background = '#0f1019',
+	selection_fg = '#8f92a0',
+	selection_bg = '#1f2131',
+}
+
 
 -- Font
 config.font = wezterm.font 'MesloLGM Nerd Font Mono'
