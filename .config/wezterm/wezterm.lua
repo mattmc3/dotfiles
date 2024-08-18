4e5a7bad7c4cd7888702cd2cac9ee618627394f4
mattmc3 <mattmc3@gmail.com>
2024-08-17 20:50:48 -0400

    Add wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,29 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Wombat'
+
+-- Font
+config.font = wezterm.font 'MesloLGM Nerd Font Mono'
+config.font_size = 14.0
+
+-- Center on screen
+wezterm.on("gui-startup", function(cmd)
+	local screen = wezterm.gui.screens().main
+	local ratio = 0.7
+	local width, height = screen.width * ratio, screen.height * ratio
+	local tab, pane, window = wezterm.mux.spawn_window(cmd or {
+		position = { x = (screen.width - width) / 2, y = (screen.height - height) / 2 },
+	})
+	-- window:gui_window():maximize()
+	window:gui_window():set_inner_size(width, height)
+end)
+
+-- and finally, return the configuration to wezterm
+return config
