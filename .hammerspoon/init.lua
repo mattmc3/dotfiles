63731f495877423180ba34a35571d4f3946e8a7a
mattmc3 <mattmc3@gmail.com>
2024-09-07 09:14:59 -0400

    hs: Refactor extend layer

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,5 +1,13 @@
 -- https://forum.colemak.com/topic/2020-for-mac-users-colemak-dh-curl-tarmak-anglewide-and-extend/
 -- https://github.com/braydenm/hyper-hacks/blob/master/hammerspoon/init.lua
+-- # http://homeowmorphism.com/2017/05/27/Remap-CapsLock-Backspace-Sierra
+-- # https://developer.apple.com/library/archive/technotes/tn2450/_index.html
+
+-- macOS map caps (0x39):
+-- * to delete (0x2A)
+-- * to F18 (0x6D)
+-- * to right opt (0xE6)
+-- hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x70000006D}]}'
 
 local hotkey = require "hs.hotkey"
 local window = require "hs.window"
@@ -13,27 +21,6 @@ local fastKeyStroke = function(modifiers, character)
   event.newKeyEvent(modifiers, string.lower(character), true):post()
   event.newKeyEvent(modifiers, string.lower(character), false):post()
 end
-
--- Enter Hyper Mode when F18 (Hyper/Capslock) is pressed
-pressedF18 = function()
-  hypermode.triggered = false
-  hypermode:enter()
-  -- hs.alert.show('in')
-end
-
--- HYPER+o: Delete
-ofun = function()
-  hs.eventtap.keyStroke({}, 'DELETE')
-  hypermode.triggered = true
-end
-hypermode:bind({}, 'o', ofun, nil, ofun)
-
--- HYPER+Semi: Forward DELETE
-semifun = function()
-  hs.eventtap.keyStroke({}, 'FORWARDDELETE')
-  hypermode.triggered = true
-end
-hypermode:bind({}, ';', semifun, nil, semifun)
 
 -- Cursor movement modifiers for line, word, selecting, etc.
 modifiers = {
@@ -49,18 +36,20 @@ modifiers = {
 
 -- Keypresses associated with each direction. I use colemak. Change to ijkl for qwerty.
 movements = {
- { 'n', 'LEFT'},
- { 'l', 'LEFT'},
- { 'e', 'DOWN'},
- { 'u', 'UP'},
- { 'i', 'RIGHT'},
- { 'y', 'RIGHT'},
- { 'j', 'PAGEUP'},
- { 'h', 'PAGEDOWN'},
+  { 'u', 'UP'},
+  { 'n', 'LEFT'},
+  { 'e', 'DOWN'},
+  { 'i', 'RIGHT'},
+  { 'l', 'HOME'},
+  { 'y', 'END'},
+  { 'j', 'PAGEUP'},
+  { 'h', 'PAGEDOWN'},
+  { 'o', 'DELETE'},
+  { ';', 'FORWARDDELETE'},
+
 }
 for j,jmod in ipairs(modifiers) do
   for i,bnd in ipairs(movements) do
-    -- hs.alert.show(i..j)
     i = function()
       -- hs.eventtap.keyStroke(jmod,bnd[2])
       fastKeyStroke(jmod,bnd[2])
@@ -70,53 +59,28 @@ for j,jmod in ipairs(modifiers) do
   end
 end
 
--- HYPER+a: Caps lock
-afun = function()
-  hs.hid.capslock.toggle()
-  hypermode.triggered = true
-end
-hypermode:bind({}, 'a', afun, nil, nil)
+hyperMaps = {
+  { 'a', function() hs.hid.capslock.toggle() end },
+  { 'k', function() hs.eventtap.leftClick(hs.mouse.absolutePosition()) end },
+  { 'm', function() hs.eventtap.rightClick(hs.mouse.absolutePosition()) end },
+  { ',', function() hs.eventtap.middleClick(hs.mouse.absolutePosition()) end },
+  { 'SPACE', function() hs.eventtap.keyStroke({}, 'RETURN') end },
+  { 1, function() hs.eventtap.keyStroke({}, 'F1') end },
+  { 2, function() hs.eventtap.keyStroke({}, 'F2') end },
+  { 3, function() hs.eventtap.keyStroke({}, 'F3') end },
+  { 4, function() hs.eventtap.keyStroke({}, 'F4') end },
+  { 5, function() hs.eventtap.keyStroke({}, 'F5') end },
+}
 
--- HYPER+k: Left Mouse Click
-kfun = function()
-  hs.eventtap.leftClick(hs.mouse.absolutePosition())
-  hypermode.triggered = true
+for _, hypMap in pairs(hyperMaps) do
+  local hypKey = hypMap[1]
+  hypfun = function()
+    print("Hyper: " .. hypKey)
+    hypMap[2]()
+    hypermode.triggered = true
+  end
+  hypermode:bind({}, hypKey, hypfun, nil, nil)
 end
-hypermode:bind({}, 'k', kfun, nil, nil)
-
--- HYPER+m: Right Mouse Click
-mfun = function()
-  hs.eventtap.rightClick(hs.mouse.absolutePosition())
-  hypermode.triggered = true
-end
-hypermode:bind({}, 'm', mfun, nil, nil)
-
--- HYPER+comma: Middle Mouse Click
-commafun = function()
-  hs.eventtap.middleClick(hs.mouse.absolutePosition())
-  hypermode.triggered = true
-end
-hypermode:bind({}, ',', commafun, nil, nil)
-
--- HYPER+space: Return
-spacefun = function()
-  hs.eventtap.keyStroke({}, 'RETURN')
-  hypermode.triggered = true
-end
-hypermode:bind({}, 'SPACE', spacefun, nil, spacefun)
-
--- HYPER+tilde: swapkeyboard layout
--- swaplayoutfun = function()
---   current = hs.keycodes.currentLayout()
---   if current == 'Colemak' then
---     hs.keycodes.setLayout('U.S.')
---   elseif current == 'U.S.' then
---     hs.keycodes.setLayout('Colemak')
---   end
---   hs.alert.show(hs.keycodes.currentLayout())
---   hypermode.triggered = true
--- end
--- hypermode:bind({}, '`', 'swapping keyboard layout', swaplayoutfun, nil, nil)
 
 -- HYPER+=: Open Google in the default browser
 equalsfun = function()
@@ -126,14 +90,18 @@ equalsfun = function()
 end
 hypermode:bind('', '=', nil, equalsfun, nil, nil)
 
+-- Enter Hyper Mode when F18 (Hyper/Capslock) is pressed
+pressedF18 = function()
+  hypermode.triggered = false
+  hypermode:enter()
+end
+
 -- Leave Hyper Mode when F18 (Hyper/Capslock) is pressed,
 --   send ESCAPE if no other keys are pressed.
 releasedF18 = function()
   hypermode:exit()
-  -- hs.alert.show('out')
   if not hypermode.triggered then
     hs.eventtap.keyStroke({}, 'ESCAPE')
-    -- hs.alert.show('ESCAPE')
   end
 end
 
@@ -147,19 +115,18 @@ cmdaltshf18 = hs.hotkey.bind({'cmd', 'alt', 'shift'}, 'F18', pressedF18, release
 
 -- Reload config when any lua file in config directory changes
 function reloadConfig(files)
-    doReload = false
-    for _,file in pairs(files) do
-        if file:sub(-4) == '.lua' then
-            doReload = true
-        end
+  doReload = false
+  for _,file in pairs(files) do
+    if file:sub(-4) == '.lua' then
+        doReload = true
     end
-    if doReload then
-        hs.reload()
-        print('reloaded')
-    end
+  end
+  if doReload then
+    hs.reload()
+    print('reloaded')
+  end
 end
 local myWatcher = hs.pathwatcher.new(os.getenv('HOME') .. '/.hammerspoon/', reloadConfig):start()
-hs.alert.show(hs.keycodes.currentLayout())
 
 -- Open WezTerm from anywhere
 -- https://gist.github.com/pythoninthegrass/f141261a0dd28a4549780e1eb0e9c0f3
