d0159a030a26fb6194362158792a0f19e9c0d846
mattmc3 <mattmc3@gmail.com>
2024-09-25 13:47:40 -0400

    Add oil

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -908,6 +908,18 @@ require('lazy').setup({
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
 
+  -- Custom ----------------------------------------------------------------------------
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  }
+  -- Custom ----------------------------------------------------------------------------
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
