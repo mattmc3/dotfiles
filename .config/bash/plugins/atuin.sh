18efab66c66f24475d2cdf47e8aea58959d0c8c1
mattmc3 <mattmc3@gmail.com>
2024-09-28 21:24:15 -0400

    Disable up arrow

--- .config/bash/plugins/atuin.sh
+++ .config/bash/plugins/atuin.sh
@@ -4,5 +4,5 @@
 if type atuin >/dev/null 2>&1; then
   # Disable up arrow with --disable-up-arrow.
   # Disabling up arrow means that we need to use C-r for search.
-  eval "$(atuin init bash)"
+  eval "$(atuin init bash --disable-up-arrow)"
 fi
