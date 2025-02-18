55703f7dd1f20a14e0f3b41e9a266d8f8b5cc0d8
mattmc3 <mattmc3@gmail.com>
2025-02-18 13:29:01 -0500

    Update git fix script

--- bin/reference/git-fix-email.sh
+++ bin/reference/git-fix-email.sh
@@ -2,20 +2,20 @@
 
 # https://help.github.com/articles/changing-author-info/
 
+# export GITFIX_OLD_EMAIL="foo@bar.com"
+# export GITFIX_NEW_EMAIL="foobar@baz.com"
+# export GITFIX_OLD_NAME="Namey Name"
+# export GITFIX_NEW_NAME="New Namey"
+
 git filter-branch --env-filter '
-
-OLD_EMAIL="$WORK_EMAIL"
-CORRECT_NAME="Matt McElheny"
-CORRECT_EMAIL="$PERS_EMAIL"
-
-if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+if [ "$GIT_COMMITTER_EMAIL" = "$GITFIX_OLD_EMAIL" ]
 then
-	export GIT_COMMITTER_NAME="$CORRECT_NAME"
-	export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+	export GIT_COMMITTER_NAME="$GITFIX_NEW_NAME"
+	export GIT_COMMITTER_EMAIL="$GITFIX_NEW_EMAIL"
 fi
-if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+if [ "$GIT_AUTHOR_EMAIL" = "$GITFIX_OLD_EMAIL" ]
 then
-	export GIT_AUTHOR_NAME="$CORRECT_NAME"
-	export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+	export GIT_AUTHOR_NAME="$GITFIX_NEW_NAME"
+	export GIT_AUTHOR_EMAIL="$GITFIX_NEW_EMAIL"
 fi
 ' --tag-name-filter cat -- --branches --tags
