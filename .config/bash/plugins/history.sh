106ed6c036bb8aa25f34d96386f6377585e98faa
mattmc3 <mattmc3@gmail.com>
2025-06-23 12:39:05 -0400

    Update history

--- .config/bash/plugins/history.sh
+++ .config/bash/plugins/history.sh
@@ -5,4 +5,4 @@ HISTTIMEFORMAT='%F %T '   # Use standard ISO 8601 timestamp.
 HISTSIZE=10000            # Remember the last x commands in memory during session
 HISTFILESIZE=100000       # Start truncating history file after x lines
 HISTCONTROL=ignoreboth    # ignoreboth is shorthand for ignorespace and ignoredups
-HISTFILE="${BASH_DATA_HOME:-$HOME}/history"
+HISTFILE="${BASH_DATA_HOME:-$HOME/.local/share/bash}/history"
