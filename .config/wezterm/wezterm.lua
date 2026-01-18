eafe7c1fe58ea1a5ad12f6948d8eb33efea9bf8f
mattmc3 <mattmc3@gmail.com>
2026-01-17 21:26:14 -0500

    Fix WezTerm window sizing

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -93,43 +93,72 @@ config.font = wezterm.font 'MesloLGM Nerd Font Mono'
 --config.font = wezterm.font 'FiraCode Nerd Font'
 config.font_size = 14.0
 
--- Rows/Cols
-config.initial_cols = 120
-config.initial_rows = 30
+local GRID = {
+  cols = 120,
+  rows = 32,
+}
+
+-- Approx cell size for MesloLGM Nerd Font @ 14pt
+local CELL = {
+  w = 8,
+  h = 18,
+}
+
+local function set_window_size(window)
+  window:set_inner_size(GRID.cols * CELL.w, GRID.rows * CELL.h)
+end
 
 -- Set the initial window start position
 wezterm.on("gui-startup", function(cmd)
-  local tab, pane, window = wezterm.mux.spawn_window(cmd or
-    {position={x=400,y=300}}
-  )
-  window:gui_window():set_position(400,300)
+  local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+  local gui = window:gui_window()
+  set_window_size(gui)
 end)
 
--- Open new windows with offset
+-- Set new windows
 -- https://github.com/wez/wezterm/issues/3173
-wezterm.on("window-config-reloaded", function(window, pane)
+wezterm.on('window-config-reloaded', function(window, pane)
   -- approximately identify this gui window, by using the associated mux id
   local id = tostring(window:window_id())
 
   -- maintain a mapping of windows that we have previously seen before in this event handler
   local seen = wezterm.GLOBAL.seen_windows or {}
-  local next_window_x = wezterm.GLOBAL.next_window_x or 450
-  local next_window_y = wezterm.GLOBAL.next_window_y or 350
-
   -- set a flag if we haven't seen this window before
   local is_new_window = not seen[id]
-
   -- and update the mapping
   seen[id] = true
   wezterm.GLOBAL.seen_windows = seen
-  wezterm.GLOBAL.next_window_x = next_window_x + 50
-  wezterm.GLOBAL.next_window_y = next_window_y + 50
 
   -- now act upon the flag
   if is_new_window then
-    window:set_position(next_window_x, next_window_y)
+    set_window_size(window)
   end
 end)
+
+
+-- wezterm.on("window-config-reloaded", function(window, pane)
+--   -- approximately identify this gui window, by using the associated mux id
+--   local id = tostring(window:window_id())
+
+--   -- maintain a mapping of windows that we have previously seen before in this event handler
+--   local seen = wezterm.GLOBAL.seen_windows or {}
+--   local next_window_x = wezterm.GLOBAL.next_window_x or 450
+--   local next_window_y = wezterm.GLOBAL.next_window_y or 350
+
+--   -- set a flag if we haven't seen this window before
+--   local is_new_window = not seen[id]
+
+--   -- and update the mapping
+--   seen[id] = true
+--   wezterm.GLOBAL.seen_windows = seen
+--   wezterm.GLOBAL.next_window_x = next_window_x + 50
+--   wezterm.GLOBAL.next_window_y = next_window_y + 50
+
+--   -- now act upon the flag
+--   if is_new_window then
+--     window:set_position(next_window_x, next_window_y)
+--   end
+-- end)
 
 -- Mimic iTerm2's floating window trick
 config.keys = {
